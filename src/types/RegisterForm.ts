2158be9aeef8e94a64a24cde8cc699a4fad4ddfd
Vinhlcse184764 <VinhLCSE184764@fpt.edu.vn>
2025-02-28 14:19:17 +0700

    sua lai form Register theo API, them giao dien thong bao ket qua dang ky

--- src/types/RegisterForm.ts
+++ src/types/RegisterForm.ts
@@ -1,15 +1,21 @@
 export interface FormData {
   fullName: string;
+  username: string;
   email: string;
   password: string;
+  passwordConfirmation: string;
   role: string;
   phone: string;
+  address: string;
 }
 
 export interface FormErrors {
   fullName?: string;
+  username?: string;
   email?: string;
   password?: string;
+  passwordConfirmation?: string;
   role?: string;
   phone?: string;
+  address?: string;
 }
